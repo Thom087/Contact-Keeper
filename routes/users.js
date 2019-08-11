1503e4241fb859c1c8c0903bf0daf8df38e3d677
Thomas Schatt <thomas.schatt@bluewin.ch>
2019-08-11 22:24:23 +0200

    Implenet user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -19,13 +20,34 @@ router.post(
       'Please enter a password with at least 6 characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        password,
+        email
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User saved!');
+    } catch (error) {
+      console.log(error.msg);
+      res.status(500).send('Server error');
+    }
   }
 );
 
