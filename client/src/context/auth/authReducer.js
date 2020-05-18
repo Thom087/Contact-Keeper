0bdf0e130c3511666ec29c101d1756650b698c6c
Thomas Schatt <thomas.schatt@bluewin.ch>
2020-05-18 12:47:05 +0200

    Before Redux

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,6 +11,13 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        laoding: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
@@ -19,8 +26,8 @@ export default (state, action) => {
         isAuthenticated: true,
         loading: false
       };
-
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
